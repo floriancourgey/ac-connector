1e323f24f4ebfd62d3238e772f10984fb59202cb
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-02-28 17:13:12 -0500

    + xtkQueryDef SelectAll

--- xtkQueryDef.js
+++ xtkQueryDef.js
@@ -15,7 +15,7 @@ class xtkQueryDef extends ACCNLObject {
       if(err) {
         return this.executeQueryReject( err );
       }
-      
+
       if(this.options.outputFormat && this.options.outputFormat.toString().toLowerCase() == "raw"){
         this.executeQueryResolve(raw);
       } else if(this.options.outputFormat && this.options.outputFormat.toString().toUpperCase() == "XML") {
@@ -33,6 +33,37 @@ class xtkQueryDef extends ACCNLObject {
       this.client.ExecuteQuery({
         sessiontoken : this.accLogin.sessionToken,
         domDoc : {$xml : query}
+      },
+        onLoaded
+      )}.bind(this, query)
+    );
+    return promise;
+  }
+
+  SelectAll( query ){
+    var promise = new Promise( (resolve, reject ) => {this.executeQueryResolve = resolve; this.executeQueryReject = reject;})
+    var onLoaded = function( err, result, raw, soapHeader ){
+      if(err){
+        return this.executeQueryReject( err );
+      }
+      if(this.options.outputFormat && this.options.outputFormat.toString().toLowerCase() == "raw"){
+        this.executeQueryResolve(raw);
+      } else if( this.options.outputFormat && this.options.outputFormat.toString().toUpperCase() == "XML"){
+        var jxonVersion = JXON.stringToJs(raw);
+        jxonVersion = jxonVersion['SOAP-ENV:Envelope']['SOAP-ENV:Body'].ExecuteQueryResponse.pdomOutput;
+        console.log('jxonVersion ? ', jxonVersion);
+        this.executeQueryResolve( JXON.jsToXml({result : jxonVersion}) );
+      } else {
+        this.executeQueryResolve( result.pdomOutput );
+      }
+    }.bind(this);
+
+    this.clientPromise.then(
+    function( query ){
+      this.client.SelectAll({
+        sessiontoken : this.accLogin.sessionToken,
+        entity : {$xml : query},
+        duplicate: false
       },
         onLoaded
       )}.bind(this, query)
