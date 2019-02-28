046f95b06b453dc6dd89cf377f48349d19c5e230
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-02-28 17:10:51 -0500

    + xtkQueryDef option 'raw' xml

--- xtkQueryDef.js
+++ xtkQueryDef.js
@@ -4,37 +4,35 @@ var soap = require('soap'),
     xtkQueryDefWSDL = require.resolve('./wsdl/wsdl_xtkquerydef.xml');
 
 class xtkQueryDef extends ACCNLObject {
-  constructor ( options ){    
+  constructor (options){
     options = options || {};
     options.wsdl = options.wsdl || xtkQueryDefWSDL;
-    super( options );
+    super(options);
   }
-  ExecuteQuery( query ){
+  ExecuteQuery(query){
     var promise = new Promise( (resolve, reject ) => {this.executeQueryResolve = resolve; this.executeQueryReject = reject;})
     var onLoaded = function( err, result, raw, soapHeader ){
-      if( err )
-      {
-        this.executeQueryReject( err );
+      if(err) {
+        return this.executeQueryReject( err );
       }
-      else
-        {
-          if( this.options.outputFormat && this.options.outputFormat.toString().toUpperCase() == "XML")
-            {
-              var jxonVersion = JXON.stringToJs(raw);
-              jxonVersion = jxonVersion['SOAP-ENV:Envelope']['SOAP-ENV:Body'].ExecuteQueryResponse.pdomOutput;
-              console.log('jxonVersion ? ', jxonVersion);
-              this.executeQueryResolve( JXON.jsToXml({result : jxonVersion}) );
-            }
-          else
-            this.executeQueryResolve( result.pdomOutput );
-        }
+      
+      if(this.options.outputFormat && this.options.outputFormat.toString().toLowerCase() == "raw"){
+        this.executeQueryResolve(raw);
+      } else if(this.options.outputFormat && this.options.outputFormat.toString().toUpperCase() == "XML") {
+        var jxonVersion = JXON.stringToJs(raw);
+        jxonVersion = jxonVersion['SOAP-ENV:Envelope']['SOAP-ENV:Body'].ExecuteQueryResponse.pdomOutput;
+        this.executeQueryResolve( JXON.jsToXml({result : jxonVersion}) );
+      } else {
+        this.executeQueryResolve( result.pdomOutput );
+      }
+
     }.bind( this );
 
     this.clientPromise.then(
     function( query ){
       this.client.ExecuteQuery({
         sessiontoken : this.accLogin.sessionToken,
-        domDoc : {$xml : query} 
+        domDoc : {$xml : query}
       },
         onLoaded
       )}.bind(this, query)
